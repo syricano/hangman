a3d10bcfbf50bfea62456f219ef224443058e73e
Anass Muhammad Ali <syriacano@gmail.com>
2023-10-06 00:50:35 +0200

    remove planning structure from the top of run.py

--- run.py
+++ run.py
@@ -1,24 +1,3 @@
-""" 
-Program Planning Structure
-1- I need a list to store words that will be used for secret guessing
-2- select one word of the list randomly. and declare attempts
-3- add input difficulty selection
-4- ask player to make an input letters
-5- validate the input to be a a letter not other types and keep prompting user
-to make input if he incorrectly input something else
-6- creating a function to check the letter if included in the secret word or if user input the whole letters of word
-update the word index values if the input is correct 
-if the input is incorrect to reduce attempts
-7- create a function to represent the current status of guessing word or incorrect attempt 
-( mainly drawing hangman symbols/character)
-8- creating a function to check if user guessed the word to declare he winned
-and ending the game, also if he lose
-9 - add a function to allow user select difficulty which will be used to pick up the long words
-in case of higher difficulty
-10 - writing game rules.
-11 - deploy the game to heroku after adding 2 build packs.
- 
-"""
 import random
 import os 
 import curses
